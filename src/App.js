15740dbb7cff5b0aaf753dda46301616c5093149
RomanMain <kohut.main@gmail.com>
2022-02-03 19:46:23 +0200

    Default setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <span style={{ color: '#dc3545' }}>Welcome to the TheFrens team. A password is required to access this node.</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
